6f5ce89279fded2ac054a89bb975021d716356be
Jordi Armadans <jordiarmadans@MacBook-Pro-de-Jordi.local>
2020-07-26 13:19:19 +0200

    Seed Format Validation

--- gameoflife.py
+++ gameoflife.py
@@ -1,7 +1,47 @@
 #imports required to read files from patterns folder
+import string
 
-#Read Input Text Pattern from File
-#Tip1: Invalid input matrix > nonzero exit status
+#Validate Format Function
+def validateFormat( str ):
+	start = True
+	seed_line_size = 0
+	for line in lines:
+		if line[0] == '!' and start:
+			#Valid Format, don't do anything
+			pass	
+
+		elif line[0] == '.' or line[0] == 'O':
+			#Change the start to false and validate all characters are valid
+			if start:
+				start = False
+				seed_line_size = len(line)-line.count('\n')	
+
+			if seed_line_size != len(line)-line.count('\n'):
+				#Invalid Format, Not Squared Matrix
+				return False
+
+			i = 1
+			while i < seed_line_size:
+				if (line[i] != '.' and line[i] != 'O'):
+					#Invalid Format, Contains Not Expected Characters
+					return False
+				i += 1	
+
+		else: 
+			#Invalid format, Unexpected Cases
+			return False
+	return True
+
+#Read Seed (Input Text Pattern) from File
+seed = open("patterns/toad.life", "r")
+lines = seed.readlines()
+
+#Validate Seed Format > Nonzero exit status
+validFormat = validateFormat(seed)
+print("Formato Valido? " + str(validFormat))
+
+
+
 
 #Create Input (original) Matrix 
 #Tip 2: Ignore lines starts with !
