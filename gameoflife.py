46654d2ab705056095027b4c03d05aefcee73b5e
Jordi Armadans <jordiarmadans@MacBook-Pro-de-Jordi.local>
2020-07-26 11:36:25 +0200

    Structure Code GameofLife

---
+++ gameoflife.py
@@ -0,0 +1,13 @@
+#imports required to read files from patterns folder
+
+#Read Input Text Pattern from File
+#Tip1: Invalid input matrix > nonzero exit status
+
+#Create Input (original) Matrix 
+#Tip 2: Ignore lines starts with !
+#Create Output (result) Matrix
+
+#Travel Input Matrix Content creating Output Matrix Content 
+#Tip3: Be careful with border rows and cols
+
+#Print Game of Life results
